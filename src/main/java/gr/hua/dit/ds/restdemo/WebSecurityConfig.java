22a8b5b2d3a46e066bd43e0d83791288ac45238b
Anargyros Tsadimas <argiris@norbloc.com>
2020-12-16 22:59:48 +0200

    added security config

---
+++ src/main/java/gr/hua/dit/ds/restdemo/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package gr.hua.dit.ds.restdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("select username,password, enabled from user where username=?")
+                .authoritiesByUsernameQuery("select username, authority from authorities where username=?");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests() // authorize
+                .antMatchers("/").permitAll() // allow "/" to be seen without authentication
+                .anyRequest().authenticated() // all requests are authenticated
+                .and()
+                .formLogin().permitAll() //allow "/login"
+                //.defaultSuccessUrl("/", true) // set default page for success login
+                .and()
+                .logout().permitAll(); // allow "logout"
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
+    }
+
+}
